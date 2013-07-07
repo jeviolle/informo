a1ed35785f7617f9b28b9167d9cc9c7bbe072e11
Rick Briganti <jeviolle@gmail.com>
2013-07-07 12:42:08 -0400

    .

---
+++ lib/informo/dpkg.rb
@@ -0,0 +1,56 @@
+module Informo
+  class DPKG
+    def initialize()
+      @@dpkgquery = "dpkg-query"
+      @@dpkgfields = ["version","architecture","maintainer","description","status"]
+    end
+
+    # return array of packages 
+    def get_packages
+      packages = Array.new
+      packages = `#{@@dpkgquery} -W -f='${Package}\n'`.split("\n")
+      return packages
+    end
+
+    def get_version(name)
+      package_version = `#{@@dpkgquery} -W -f='${Version}' #{name}`
+      return package_version
+    end
+
+    def get_architecture(name)
+      package_architecture = `#{@@dpkgquery} -W -f='${Architecture}' #{name}`
+      return package_architecture
+    end
+
+    def get_maintainer(name)
+      package_maintainer = `#{@@dpkgquery} -W -f='${Maintainer}' #{name}`
+      return package_maintainer
+    end
+
+    def get_short_description(name)
+      package_description = `#{@@dpkgquery} -W -f='${Description}' #{name}`.split("\n")
+      return package_description[0]
+    end
+
+    def get_description(name)
+      package_description = `#{@@dpkgquery} -W -f='${Description}' #{name}`
+      return package_description
+    end
+
+    def get_status(name)
+      package_status = `#{@@dpkgquery} -W -f='${Status}' #{name}`
+      return package_status
+    end
+
+    def get_all(name)
+      results = `#{@@dpkgquery} -W -f='${Version}|||${Architecture}|||${Maintainer}|||${Description}|||${Status}' #{name}`.split('|||')
+
+      # return results as an array and map
+      hresults = {'name' => name, 'version' => results[0],
+                  'architecture' => results[1], 'maintainer' => results[2],
+                  'description' => results[3], 'status' => results[4]}
+
+      return hresults
+    end
+  end
+end
---
+++ lib/informo/rpm.rb
@@ -0,0 +1,60 @@
+module Informo
+  class RPM
+    def initialize()
+      @@rpmquery = "rpm"
+    end
+
+    # return array of packages 
+    def get_packages
+      packages = Array.new
+      packages = `#{@@rpmquery} -qa --qf='%{NAME}\n'`.split("\n")
+      return packages
+    end
+
+    def get_version(name)
+      package_version = `#{@@rpmquery} -q --qf='%{VERSION}' #{name}`
+      return package_version
+    end
+
+    def get_release(name)
+      package_release = `#{@@rpmquery} -q --qf='%{RELEASE}' #{name}`
+      return package_release
+    end
+
+    def get_architecture(name)
+      package_architecture = `#{@@rpmquery} -q --qf='%{ARCH}' #{name}`
+      return package_architecture
+    end
+
+    def get_maintainer(name)
+      package_maintainer = `#{@@rpmquery} -q --qf='%{VENDOR}' #{name}`
+      return package_maintainer
+    end
+
+    def get_short_description(name)
+      package_description = `#{@@rpmquery} -q --qf='%{SUMMARY}' #{name}`.split("\n")
+      return package_description[0]
+    end
+
+    def get_description(name)
+      package_description = `#{@@rpmquery} -q --qf='%{DESCRIPTION}' #{name}`
+      return package_description
+    end
+
+    def get_installdate(name)
+      package_status = `#{@@rpmquery} -q --qf='%{INSTALLTIME:date}' #{name}`
+      return package_status
+    end
+
+    def get_all(name)
+      results = `#{@@rpmquery} -q --queryformat "%{VERSION}|||%{RELEASE}|||%{ARCH}|||%{VENDOR}|||%{SUMMARY}|||%{DESCRIPTION}|||%{INSTALLTIME:date}" #{name}`.split('|||')
+
+      # return results as an array and map
+      hresults = {'name' => name, 'version' => results[0], 'release' => results[1],
+                  'architecture' => results[2], 'maintainer' => results[3], 'summary' => results[4],
+                  'description' => results[5], 'installdate' => results[6]}
+
+      return hresults
+    end
+  end
+end
